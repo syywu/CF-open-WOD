eb9b5b4da043bb8b4a3a32672a00c790dbd88a69
Samantha Wu <samanthayywu@gmail.com>
2022-05-28 17:51:13 +0100

    ejs

--- app.js
+++ app.js
@@ -8,12 +8,28 @@ const PORT = process.env.PORT || "3000";
 const app = express();
 const url = "https://www.crossfit.com/workout";
 
+app.set("view engine", "ejs");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.static("public"));
 app.use("/api/wod", router);
 
-axios(url);
+app.get("/", (req, res) => {
+  res.render("index", { title: "home" });
+});
+
+// axios(url)
+//   .then((response) => {
+//     const html = response.data;
+//     const $ = cheerio.load(html);
+//     const wod = [];
+//     $("wodRaw", html).each(function () {
+//       const text = $(this).text();
+//       wod.push({ text });
+//     });
+//     console.log(wod);
+//   })
+//   .catch((err) => console.log(err));
 
 app.listen(PORT, () => {
   console.log(`listening to port ${PORT}`);
