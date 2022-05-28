a406425a8236887f1baba19878b2ddb13746e346
Samantha Wu <samanthayywu@gmail.com>
2022-05-28 16:21:14 +0100

    router

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cheerio from "cheerio";
 import axios from "axios";
 import logger from "morgan";
+import router from "./routes/wod.js";
 
 const PORT = process.env.PORT || "3000";
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.static("public"));
+app.use("/api/wod", router);
 
 app.listen(PORT, function () {
   console.log(`listening to port ${PORT}`);
