0a742aacd79f1638aa8d5111f29d76bc079f878b
Samantha Wu <samanthayywu@gmail.com>
2022-06-05 17:06:20 +0100

    connected to db

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import router from "./routes/wod.js";
 
 const PORT = process.env.PORT || "3000";
 const app = express();
-const url = "https://www.crossfit.com/workout?page=";
+// const url = "https://www.crossfit.com/workout?page=";
 
 app.set("view engine", "ejs");
 app.use(logger("dev"));
@@ -14,33 +14,33 @@ app.use(express.json());
 app.use(express.static("public"));
 app.use("/api/wod", router);
 
-let wod = [];
+// let wod = [];
 
 app.get("/", (req, res) => {
   res.render("index", { title: "home" });
 });
 
-app.get("/wod", (req, res) => {
-  res.json(getWorkoutUrl());
-});
+// app.get("/wod", (req, res) => {
+//   res.json(getWorkoutUrl());
+// });
 
-function getWorkoutUrl() {
-  let url = [];
-  for (let i = 1; i < 5; i++) {
-    let page = url + i.toString();
-    axios
-      .get(page, { headers: { accept: "application/json" } })
-      .then((response) => {
-        let data = response.data;
-        // let data = JSON.parse(response.data);
-        for (let i = 0; i < data.wods.length; i++) {
-          url.push(data.wods[i].url);
-        }
-      })
-      .catch((err) => console.log(err));
-  }
-  return url;
-}
+// function getWorkoutUrl() {
+//   let url = [];
+//   for (let i = 1; i < 5; i++) {
+//     let page = url + i.toString();
+//     axios
+//       .get(page, { headers: { accept: "application/json" } })
+//       .then((response) => {
+//         let data = response.data;
+//         // let data = JSON.parse(response.data);
+//         for (let i = 0; i < data.wods.length; i++) {
+//           url.push(data.wods[i].url);
+//         }
+//       })
+//       .catch((err) => console.log(err));
+//   }
+//   return url;
+// }
 
 // axios(url)
 //   .then((response) => {
