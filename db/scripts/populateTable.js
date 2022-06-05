398895d2bbbc89a52273b7bdb10cd6b35b8c8e05
Samantha Wu <samanthayywu@gmail.com>
2022-06-05 18:06:21 +0100

    populateTable

--- db/scripts/populateTable.js
+++ db/scripts/populateTable.js
@@ -1,2 +1,19 @@
-import { workoutOfTheDay } from "../../obj";
-import query from "../index.js";
+import { workoutOfTheDay } from "../../obj.js";
+import { query } from "../index.js";
+
+async function populateTable() {
+  for (let i = 0; i < workoutOfTheDay.length; i++) {
+    const res = await query(
+      `INSERT INTO wod (name, time , rounds, wod) VALUES ($1, $2, $3, $4) RETURNING *`,
+      [
+        workoutOfTheDay[i].name,
+        workoutOfTheDay[i].time,
+        workoutOfTheDay[i].rounds,
+        workoutOfTheDay[i].wod,
+      ]
+    );
+    console.log(res.rows[0]);
+  }
+}
+
+populateTable();
