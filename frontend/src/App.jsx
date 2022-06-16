924687652cb9fda11f3c15b9db871793259fa58e
Samantha Wu <samanthayywu@gmail.com>
2022-06-16 16:40:31 +0100

    formatting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,36 +1,33 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import Display from './Display'
+import { useState, useEffect } from "react";
+import "./App.css";
+import Display from "./Display";
 
 function App() {
+  const [isPending, setisPending] = useState(true);
+  const [workouts, setWorkouts] = useState(null);
 
-    const [isPending, setisPending] = useState(true)
-    const [workouts, setWorkouts] = useState(null)
-    
-
-    async function getData(){
-      let res = await fetch('http://localhost:8000/api/wod')
-      let data = await res.json()
-      return data.payload
-    }
-   
-  async function handleClick(){
-    let data = await getData()
-    setWorkouts(data)
-    console.log(data)
+  async function getData() {
+    let res = await fetch("http://localhost:8000/api/wod");
+    let data = await res.json();
+    return data;
   }
 
+  async function handleClick() {
+    let data = await getData();
+    setWorkouts(data);
+    console.log(data);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-     <Display workout={workouts}/>
-          <button type="button" onClick={() => handleClick()}>
-            Next WOD
-          </button>
+        <Display workouts={workouts} />
+        <button type="button" onClick={() => handleClick()}>
+          Next WOD
+        </button>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/Display.jsx
+++ frontend/src/Display.jsx
@@ -1,9 +1,13 @@
-const Display = ({workouts}) => {
-    return (  
-        <div className="display">
-            {workouts}
-            </div>
-    );
-}
- 
+const Display = ({ workouts }) => {
+  return (
+    <div className="display">
+      {workouts.map((workout) => (
+        <div>
+          <p>{workout.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
 export default Display;
